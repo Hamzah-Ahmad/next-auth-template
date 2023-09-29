cb33584fd96973b318046f126e416d06e0f0cbb4
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2023-09-28 20:21:38 -0500

    feat: responsiveness fixes

--- app/(routes)/protected/page.tsx
+++ app/(routes)/protected/page.tsx
@@ -8,7 +8,7 @@ const Protected = () => {
       <Header />
 
       <div className="flex  h-96 flex-col items-center justify-end">
-        <div className="text-xl">This page can only be visisted by authenticated users</div>
+        <div className="text-center text-lg md:text-xl">This page can only be visisted by authenticated users</div>
         <Link href="/" className="mt-4 text-white p-2 rounded-lg bg-neutral-950">
           Go To Main Page
         </Link>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,8 +7,8 @@ const Header = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className="flex justify-end h-12 mr-14 pt-4 gap-3 items-center">
-      <div className="text-xl">
+    <div className="flex justify-center md:justify-end h-12 mr-0 md:mr-14 pt-4 gap-3 items-center">
+      <div className="text-base md:text-xl">
         Welcome {session ? session.user?.name : "Guest"}
       </div>
       <AuthButton />
